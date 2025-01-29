0df1a772df3c6cbf2ef2f4582e683e738a4c8506
AvneetPama <152096616+AviPama@users.noreply.github.com>
2025-01-29 11:02:49 -0800

    Update IAM.tf

--- IAM.tf
+++ IAM.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-east-1" # Specify your AWS region
+  region = "us-east-1" 
 }
 
 # Create an IAM group
@@ -26,23 +26,19 @@ resource "aws_iam_group_policy" "example_group_policy" {
   })
 }
 
-# Create an IAM user
 resource "aws_iam_user" "example_user" {
   name = "example_user"
 }
 
-# Add the user to the group
 resource "aws_iam_user_group_membership" "example_user_group_membership" {
   user = aws_iam_user.example_user.name
   groups = [aws_iam_group.example_group.name]
 }
 
-# Optionally, create an access key for the user
 resource "aws_iam_access_key" "example_access_key" {
   user = aws_iam_user.example_user.name
 }
 
-# Output the access key and secret (handle securely in real scenarios)
 output "access_key" {
   value = aws_iam_access_key.example_access_key.id
   sensitive = true
